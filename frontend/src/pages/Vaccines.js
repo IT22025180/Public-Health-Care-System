1dc5b32024ce0491398b16db8f1327fa1bda7df2
IT22181688 <it22181688@my.sliit.lk>
2024-03-18 13:37:00 +0530

    Hima

---
+++ frontend/src/pages/Vaccines.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Layout from '../components/Layout'
+
+const Vaccines = () => {
+    return(
+        <Layout>
+            <div>
+                <h2>Vaccines</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Ex date</th>
+
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </Layout>
+    )
+}
+export default Vaccines;
