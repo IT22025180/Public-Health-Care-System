45c826c4ce628ad4fa7bc466ae28bbe18dc19fcc
vanuja <vinnathvanuja@gmail.com>
2024-04-01 16:43:56 +0530

    Vanuja

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,6 @@ function App() {
         <Route path='/Fine-And-court-Document-Management' element = {<FCDocManage/>}/>
         <Route path='/Fine-And-court-Analyse' element = {<FCAnalyse/>}/>
         <Route path='/clinics' element = {<Clinics/>}/>
-
-
       </Routes>
     </Router>
   );
