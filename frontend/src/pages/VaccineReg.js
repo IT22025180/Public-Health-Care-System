0aa6ce347d9d9046bec392c1620defe618a7a9d4
IT22181688 <it22181688@my.sliit.lk>
2024-04-02 14:55:22 +0530

    hima

--- frontend/src/pages/VaccineReg.js
+++ frontend/src/pages/VaccineReg.js
@@ -13,24 +13,24 @@ const VaccineReg = () => {
     <h2 className="he2" >Vaccine Registration</h2>
     <form className='addvaccine'>
         <div className='input'>
-            <label htmlFor='vname'>Vaccine Name</label>
+            <label htmlFor='vname'>Vaccine Name :</label>
             <input type='text' id='vname' autoComplete='off' placeholder='Vaccine Name'/>
         </div>
 
 
         <div className='input'>
-            <label htmlFor='ManDate'>Manufactured date</label>
+            <label htmlFor='ManDate'>Manufactured date :</label>
             <input type='date' id='ManDate' autoComplete='off' placeholder='Manufactured date'/>
         </div>
 
         <div className='input'>
-            <label htmlFor='ExDate'>Expiration date</label>
+            <label htmlFor='ExDate'>Expiration date :</label>
             <input type='date' id='ExDate' autoComplete='off' placeholder='Expiration date'/>
         </div>
 
 
         <div className='input'>
-            <label htmlFor='specialnotes'>Special Notes</label>
+            <label htmlFor='specialnotes'>Special Notes :</label>
             <input type='text' id='specialnotes' autoComplete='off' placeholder='Special Notes'/>
         </div>
 
