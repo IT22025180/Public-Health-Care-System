4aa3777e03e5e3f6cb963916a43b441fef1c9205
YethmiLiyanaarachchi <it22181992@my.sliit.lk>
2024-04-02 10:58:05 +0530

    Yethmi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Staff from './pages/Staff';
+import { Sidebar } from "react-pro-sidebar";
+import Leave from './pages/Leave';
+import  Allschedules  from './pages/Allschedules';
+
+
 
 function App() {
   return (
@@ -8,6 +13,10 @@ function App() {
       <Routes>
         <Route path='/' element = {<Home/>}/>
         <Route path='/staff' element = {<Staff/>}/>
+        <Route path='/Leave' element = {<Leave/>}/>
+        <Route path='/Allschedules' element = {<Allschedules/>}/>
+
+
       </Routes>
     </Router>
   );
