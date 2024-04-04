3bf5296ef1d06b41686e6473b873512f983537f4
IT22087560 <IT22087560@my.sliit.lk>
2024-04-04 13:12:29 +0530

    manuji

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ import Bvaccinetable from './pages/Bvaccinetable';
 import Thriposhatable from './pages/Thriposhatable';
 import AddPatients from './pages/AddPatients';
 import DengueCampaigns from './pages/DengueCampaignSchedule';
-
+import DengCampTab from './pages/DengCampTab';
 
 function App() {
   return (
@@ -51,7 +51,7 @@ function App() {
         <Route path='/Babytable' element={<Babytable/>}/>
         <Route path='/Bvaccinetable' element={<Bvaccinetable/>}/>
         <Route path='/Thriposhatable' element={<Thriposhatable/>}/>
-
+        <Route path='/DengCampTab' element={<DengCampTab/>}/>
 
 
 
