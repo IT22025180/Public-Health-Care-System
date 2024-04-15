a4b738bbe9232924485e8e134b43392d16b47037
Hima Alahakoon <it22181688@my.sliit.lk>
2024-04-16 00:45:46 +0530

    hima

--- frontend/src/pages/VaccineAppTab.js
+++ frontend/src/pages/VaccineAppTab.js
@@ -4,6 +4,8 @@ import Axios from 'axios';
 import Swal from 'sweetalert2';
 import Layout from '../components/Layout';
 import {  useNavigate } from 'react-router-dom';
+import jsPDF from 'jspdf';
+import logo1 from '../webImages/logo1.png'; 
 
 const VaccineAppTab = () => {
     //state variables
@@ -64,13 +66,75 @@ const VaccineAppTab = () => {
     });
 }
 
+   //pdf generation  
+
+   const generatePDF = (vaccineapp) => {
+    const doc = new jsPDF();
+
+    // Add Sri Lankan national logo
+    const logo = new Image();
+    logo.src = logo1; // Use the imported logo image
+    doc.addImage(logo, 'PNG', 6, 7, 20, 20); // Adjust the position and dimensions as needed
+
    
+    // Add Public Health Information System as the letterhead
+    doc.setFontSize(12);
+    doc.text('Public Health Information System', 70, 15); // Adjust the position as needed
+    doc.text('Suwasiripaya, No. 385, Rev. Baddegama Wimalawansa Thero Mawatha,', 70, 20);
+    doc.text('Colombo 10, Sri Lanka.', 70, 25);
+    doc.text('Tel: 112 694033, 112 675011, 112 675449, 112 693493', 70, 30);
+
+    // Add page border
+    doc.setDrawColor(0);
+    doc.setLineWidth(0.5);
+    doc.rect(0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height, 'S');
+
+    // Add horizontal line
+    doc.setLineWidth(0.5);
+    doc.line(5, 45, 205, 45);
+
+    // Vaccine registration summary topic
+    doc.setFontSize(18);
+    doc.setTextColor(0, 0, 0); // Set text color to black
+    doc.text('Notice-Vaccination Programme', 70, 60); // Adjust the position as needed
+
+    //appointment description
+
+    const AppDescription =`
+    There will be a crucial vaccination program scheduled on ${vaccineapp.date} at ${vaccineapp.location}. 
+    This program plays a pivotal role in our ongoing efforts to combat infectious diseases by distributing 
+    ${vaccineapp.v_name} vaccine to the designated patients. The successful execution of this initiative
+     relies heavily on the coordinated efforts of our dedicated team.
+
+    It is imperative that all assigned staff members arrive promptly before 8:00 AM
+    to ensure the smooth operation of the event. Their presence and active participation
+    are vital in administering vaccines efficiently and providing necessary assistance to 
+    patients.
+
+    Furthermore, proper preparation and adherence to safety protocols are paramount to guaranteeing 
+    the health and well-being of both staff and patients. This includes maintaining 
+    strict hygiene standardsensuring proper storage and handling of vaccines, and following 
+    established procedures for patient care.
+    
+    `;
+
+    doc.setFontSize(12);
+    doc.text(AppDescription, 15, 75);
+
+    // Date and signature
+    const currentDate = new Date().toLocaleDateString('en-US');
+    doc.setFontSize(12);
+    doc.text(`Date: ${currentDate}`, 15, 170); 
+    doc.text('Signature:', 15, 180); 
+
+    // Save the PDF with a filename based on leave name
+    doc.save(`Vaccine Appointment_${vaccineapp.v_name}.pdf`);
 
 
 
 
   
-
+   };
   return (
     <Layout>
     <div className='VaccineAppTab'>
@@ -86,6 +150,7 @@ const VaccineAppTab = () => {
                     <th>Location</th>
                     <th>Update</th>
                     <th>Delete</th>
+                    <th>PDF</th>
                 </tr>
             </thead>
             <tbody>
@@ -110,6 +175,11 @@ const VaccineAppTab = () => {
                         <td onClick={() => deletevaccineappdata(vaccineapp._id)} className='deleteButtons'>
                             <button >Delete</button>
                         </td>
+                         
+                        <td> 
+                        <button className="pdfButton" onClick={() => generatePDF(vaccineapp)}>Generate PDF</button>
+                        </td>
+                  
     
                     </tr>
 
