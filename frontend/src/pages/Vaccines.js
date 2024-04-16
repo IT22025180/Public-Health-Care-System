7a232e3f192f00650051d4c35556e50dff052aee
Hima Alahakoon <it22181688@my.sliit.lk>
2024-04-16 19:58:55 +0530

    hima

--- frontend/src/pages/Vaccines.js
+++ frontend/src/pages/Vaccines.js
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 import image1 from '../webImages/vac1.jpg'
 import image2 from '../webImages/vac2.jpg'
 import image3 from '../webImages/vac3.jpg'
+import vaccmain from '../webImages/vaccmain.png';
 
 const Vaccines = () => {
     
@@ -66,6 +67,23 @@ const Vaccines = () => {
               </div>
             </div>
           ))}
+        </div>
+
+        <div className="vachome">
+            <div className="vhome">
+                <h2>Vaccination Details </h2>
+                <h3>Check all the vaccination data and distributions</h3>
+                <p><center>Vaccination distribution and management encompass a complex framework of planning, 
+                  coordination, and execution to ensure the effective delivery of vaccines to target populations.<br></br>
+                   This involves meticulous supply chain management to maintain vaccine integrity,
+                    allocation strategies to prioritize high-risk groups, and the establishment of distribution networks spanning diverse healthcare settings.<br></br>
+                     Additionally, robust monitoring, surveillance, and community engagement efforts are essential to track vaccine uptake,
+                      address concerns, and adapt strategies as needed, ultimately fostering widespread vaccine acceptance and protecting public health.<br></br>
+                      <button onClick={() => navigate('/VaccineRegTab')} className="view-details-btn">View Details</button>
+
+</center></p>
+            </div>
+            <img src={vaccmain} alt="Description of the image" className="vaccmain" />
         </div>
           
           
