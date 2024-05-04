ff8b7a1dafcb08a1d41174f2b6a33da1878a3254
IT22087560 <IT22087560@my.sliit.lk>
2024-05-04 19:22:14 +0530

    manuji

--- frontend/src/pages/Editcampdetails.js
+++ frontend/src/pages/Editcampdetails.js
@@ -79,7 +79,7 @@ const Editcampdetails = () => {
         }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-            Swal.fire("Saved!","Clinic updated successfully !! ", "success");
+            Swal.fire("Saved!","Campaign updated successfully !! ", "success");
             update();
         } else if (result.isDenied) {
             Swal.fire("Changes are not saved", "", "info");
