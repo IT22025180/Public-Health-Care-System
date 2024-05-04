a38e0a89316aec59525c1fafc656d621ee09a73e
it22924278 <it22924278@my.sliit.lk>
2024-05-04 21:09:15 +0530

    vihanga

--- frontend/src/pages/Complains.js
+++ frontend/src/pages/Complains.js
@@ -278,7 +278,7 @@ const ComplaintForm = () => {
         <Container>
           <h1>Public Health Complaint Form</h1>
 
-          <Form>
+          <Form className="comform">
             <Row>
               <Col>
                 <Form.Group controlId="firstName">
